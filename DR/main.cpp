7ef29efe936ad7b585bcf8fcf98e5eff15b4d215
Gerric Duncan <gerricduncan@outlook.com>
2015-05-04 10:21:50 -0400

    Initial Commit

---
+++ DR/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DR
+//
+//  Created by Gary Duncan on 5/4/15.
+//  Copyright (c) 2015 Gary Duncan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
